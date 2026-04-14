76dae0dc24be28676caade7586b7918c11968570
293459 <s293459@studenti.polito.it>
2026-04-14 12:11:20 +0200

    rebuild con codex, file json modificati

--- 3-Paletta/calcolo_area.f90
+++ 3-Paletta/calcolo_area.f90
@@ -2,37 +2,38 @@ subroutine calcolo_area(coords, A)
 
     implicit none
 
-    real(4), dimension(3,2) :: coords ! 3 vertici e due coordinate x e y
-    real (4):: A, p ! semiperimetro e area
-    real(4), dimension(3) :: lat ! lati del triangolo
-    integer :: i ! contatore
+    real(4), intent(in), dimension(3,2) :: coords
+    real(4), intent(out) :: A
+    real(4) :: p
+    real(4), dimension(3) :: lat
+    integer :: i, j
 
-    ! ricordiamo che noi abbiamo le coordinate dei punti ma non la lunghezza dei lati già fornita
+    ! In ingresso abbiamo le coordinate dei tre vertici.
+    ! Per usare la formula di Erone dobbiamo prima ricavare
+    ! la lunghezza dei tre lati del triangolo.
+    lat = 0.
+    A = 0.
 
-    lat = 0. ! inizializzo i lati a zero, così posso sommare le lunghezze dei lati e poi calcolare il semiperimetro e l'area
-    A = 0. ! inizializzo l'area a zero, così posso calcolarla con la formula di Erone dopo aver calcolato i lati e il semiperimetro
-    do i=1,3
+    do i = 1,3
 
-        if (i /= 3) then ! se i è diverso da 3, calcolo la lunghezza del lato tra il vertice i e il vertice i+1
-
-                lat(i) = sqrt((coords(i,1)-coords(i+1,1))**2+(coords(i, 2)-coords(i+1, 2))**2)
-
-        else ! se i è uguale a 3, calcolo la lunghezza del lato tra il vertice 3 e il vertice 1
-                lat(i) = sqrt((coords(i,1)-coords(1,1))**2+(coords(i, 2)-coords(1, 2))**2)
-                
-        ! metto questa if condition perché altrimenti quando i è uguale a 3, il programma cerca di accedere alla riga 4 della matrice coords, 
-        ! che non esiste, e quindi dà un errore di out of bounds, invece con questa if condition, quando i è uguale a 3, il programma accede 
-        ! alla riga 1 della matrice coords, che esiste, e quindi non dà errore
-
+        if (i /= 3) then
+            j = i + 1
+        else
+            j = 1
         end if
+
+        ! Per i primi due lati considero il vertice successivo.
+        ! Per l'ultimo lato torno al primo vertice e chiudo il triangolo.
+        lat(i) = sqrt((coords(i,1) - coords(j,1))**2 + (coords(i,2) - coords(j,2))**2)
 
     end do
 
-    p=lat(1)+lat(2)+lat(3) ! calcolo il perimetro del triangolo sommando i lati
+    ! Il semiperimetro e la meta della somma dei tre lati.
+    p = (lat(1) + lat(2) + lat(3)) / 2.0
 
-    p=p/2.0 ! calcolo il semiperimetro del triangolo dividendo il perimetro per 2
-
-    A=sqrt(p*(p-lat(1))*(p-lat(2))*(p-lat(3))) 
-    ! calcolo l'area del triangolo con la formula di Erone, che è A = sqrt(p*(p-a)*(p-b)*(p-c)), dove p è il semiperimetro e a, b, c sono i lati del triangolo
+    ! Applico la formula di Erone:
+    ! A = sqrt(p * (p-a) * (p-b) * (p-c))
+    ! dove a, b e c sono le tre lunghezze dei lati.
+    A = sqrt(p * (p - lat(1)) * (p - lat(2)) * (p - lat(3)))
 
 end subroutine
--- 3-Paletta/calcolo_baricentro.f90
+++ 3-Paletta/calcolo_baricentro.f90
@@ -1,43 +1,26 @@
 subroutine calcolo_baricentro(coords, x_b)
 
     implicit none
-    ! la funzione è tunata per il caso specifico ma manca di generalità perché 
-    ! è specifica per il triangolo, se volessimo fare un quadrato dovremmo cambiare 
-    ! la formula e quindi non sarebbe più una subroutine riutilizzabile
 
-    ! se volessimo fare una subroutine più generale dovremmo scrivere una subroutine
-    ! che prende in ingresso il numero di vertici e la matrice delle coordinate e poi
-    ! fare un ciclo che somma tutte le righe della matrice e alla fine divide per il 
-    ! numero di vertici, in questo modo avremmo una subroutine più generale che potrebbe 
-    ! essere riutilizzata per qualsiasi poligono (ma qui semplifichiamo)
-    real(4), dimension(3,2) :: coords ! 3 vertici e due coordinate x e y
-    real(4), dimension(2) :: x_b ! baricentro (ha 2 coordinate)
+    ! Questa subroutine calcola il baricentro di un triangolo piano.
+    ! Nel caso della esercitazione basta fare la media delle coordinate
+    ! dei tre vertici.
+    !
+    ! La routine e pensata in modo semplice e specifico per il triangolo.
+    ! Se in futuro si volesse gestire un poligono con N vertici,
+    ! sarebbe sufficiente sommare tutte le coordinate e dividere per N.
+    real(4), intent(in), dimension(3,2) :: coords
+    real(4), intent(out), dimension(2) :: x_b
     integer :: i
 
-
-    x_b=0. ! inizializzo il baricentro a zero, così posso sommare le coordinate dei vertici e poi dividere per 3
-    do i=1,3
-        x_b= x_b+coords(i,:)   !prendo una riga la metto in xb e la sommo all'iterata successiva a quella dopo, cos� contemporaneamente x e y crescono
+    ! Inizializzo il baricentro a zero e accumulo i contributi
+    ! dei tre vertici sia in x sia in y.
+    x_b = 0.
+    do i = 1,3
+        x_b = x_b + coords(i,:)
     end do
-    x_b=x_b/3 ! alla fine divido per 3 perché ho 3 vertici, se avessi n vertici dovrei dividere per n
 
-! quella inserita tra commenti di seguito è la versione più generale    
-!   somma= 0.
-!   x_b = 0.
-
-!    do j=1,2
-
-!        do i=1,3
-!           somma(j)=somma(j)+coords(i,j)
-!       end do
-
-!    end do
-
-!    x_b=somma/3;
-
-
+    ! Divido per il numero di vertici per ottenere la media.
+    x_b = x_b / 3.
 
 end subroutine
-
-
-
