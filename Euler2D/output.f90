a17d826967b47dcb2b7c477cbf9f70e806989ab1
293459 <s293459@studenti.polito.it>
2026-05-03 11:06:59 +0200

    aggiunto al file output f90 la possibilità di creare cartelle ad ogni iterazione con i risultati della simulazione e il file di recup

--- Euler2D/output.f90
+++ Euler2D/output.f90
@@ -8,8 +8,21 @@ subroutine write_tecplot_file(k_file,traslay)
  real(4),dimension(2)::csi,xy,xyloc
 logical::trovato
 
+!--------------------------------------------------------------------------------
+! blocco opzionale aggiunto da me per creare una cartella per ogni iterazione
+! 1. CREA IL NOME DELLA CARTELLA (es: Results_Iter_100)
+ write(kcar,'(I0)') k_file ! I0 elimina gli spazi vuoti automaticamente
+ folder_name = "SIM_OUTPUT_" // trim(kcar)
 
+ ! 2. CREA LA CARTELLA FISICAMENTE (Comando di sistema)
+ ! 'mkdir -p' funziona su Linux/Mac. Se sei su Windows usa 'mkdir'
+ call execute_command_line("mkdir -p " // trim(folder_name))
 
+ ! 3. DEFINISCI IL PERCORSO DEL FILE DENTRO LA CARTELLA
+ datafile_tec = trim(folder_name) // "/" // trim(kcar) // ".plt"
+!--------------------------------------------------------------------------------
+
+!--------------------------------------------------------------------------------
 
  	if(k_file.lt.10) then
 	write(kcar,'(i1)') k_file
@@ -29,65 +42,49 @@ logical::trovato
 	write(kcar,'(i8)') k_file
 	else if(k_file.lt.1000000000) then
 	write(kcar,'(i9)') k_file
-	end if
-
-
-      iposj2=index(kcar,' ')
-
-	datafile_tec=kcar(1:iposj2-1)//'.plt'
-
-
-
+    end if iposj2=index(kcar,' ')! trova la posizione del primo spazio vuoto, in questo modo riesco a prendere solo la parte numerica del nome del file, senza spazi vuoti
+	datafile_tec=kcar(1:iposj2-1)//'.plt' ! creo il nome del file tecplot, con estensione .plt, usando solo la parte numerica di kcar
 
 !--------------------------------------------------------------------------------
- open(unit=1, file=datafile_tec)
 
- write(1,*) 'title = "grid"'
-
-
- write(1,'(a80)') 'variables = "x","y","rho","P","u","v","M","S"'
-
- write(1,*) 'zone n=',nnodi,' e =', nele_interni,' et=quadrilateral, f=fepoint'
+ open(unit=1, file=datafile_tec) ! apertura del file tecplot, il nome del file è quello creato sopra
+ write(1,*) 'title = "grid"' ! titolo del file tecplot
+ write(1,'(a80)') 'variables = "x","y","rho","P","u","v","M","S"' 
+ ! variabili da salvare su file, devono essere in accordo con quelle che si scrivono dopo, 
+ ! se si vuole salvare una variabile in più o in meno basta aggiungerla o toglierla da questa riga 
+ ! e da quella dopo, facendo attenzione all'ordine
+ write(1,*) 'zone n=',nnodi,' e =', nele_interni,' et=quadrilateral, f=fepoint' 
+ ! specifica che il file contiene una zona con nnodi nodi, nele_interni elementi, tipo quadrilatero e formato fepoint
 
 !--------------------------------------------------------------------------------
+
 ! Nodal quantities: x, y, rho, u, v, p, Mach number
+   do i = 1, nnodi ! ciclo su tutti i nodi, in questo ciclo si scrivono le variabili da salvare su file per ogni nodo
 
+    rho_node=0.        ! inizializzo a zero la densità al nodo
+    rhou_node=0.       ! inizializzo a zero la quantità di moto in x al nodo
+    rhov_node=0.       ! inizializzo a zero la quantità di moto in y al nodo
+    e_node=0.          ! inizializzo a zero l'energia totale al nodo
+    muSA_node=0.       ! inizializzo a zero la viscosità turbolenta al nodo
+    wall_dist_node=0.  ! inizializzo a zero la distanza dalla parete al nodo
+    mua_node=0.        ! inizializzo a zero la viscosità dinamica al nodo
+    vort_thick_node=0. ! inizializzo a zero il vortice di spessore al nodo
+    u6_node=0.         ! inizializzo a zero la variabile u6 al nodo (residuo totale)
+    somma=0.           ! inizializzo a zero la variabile somma, che serve per fare la media delle variabili al nodo, in caso il nodo sia condiviso da più elementi
 
-
-   do i = 1, nnodi
-
-    rho_node=0.
-    rhou_node=0.
-    rhov_node=0.
-    e_node=0.
-    muSA_node=0.
-    wall_dist_node=0.
-    mua_node=0.
-    vort_thick_node=0.
-    u6_node=0.
-    u7_node=0.
-    u8_node=0.
-    somma=0.
-
-
-
-    trovato=.FALSE.
-    do j=1,nnodi_vis
-    if(i.eq.nodi_vis(j)) then
+    trovato=.FALSE. ! inizializzo a falso la variabile trovato, che serve per capire se il nodo è un nodo di interfaccia o meno
+    do j=1,nnodi_vis ! ciclo sui nodi di interfaccia, che sono quelli che devo salvare su file
+    if(i.eq.nodi_vis(j)) then ! se il nodo i è un nodo di interfaccia allora lo salvo su file, altrimenti no
         trovato=.TRUE.
         exit
     end if
     end do
 
-    if(trovato)then
+    if(trovato)then !
         !do j=1,1
         do j=1,nodo(i)%neles
             if((nodo(i)%ele(j).ne.0))then
             
-  
-
-                
-                
                 rho_node=rho_node+ele(nodo(i)%ele(j))%ucons(2)
                 rhou_node=rhou_node+ele(nodo(i)%ele(j))%ucons(3)
                 rhov_node=rhov_node+ele(nodo(i)%ele(j))%ucons(4)
@@ -128,16 +125,11 @@ logical::trovato
 
     end if
 
-
-
             write(1,*) nodo(i)%x(1), nodo(i)%x(2)+traslay,rho_node,P_node,u_node,v_node,M_node,S_node
-
-
-
-
-
-
-
+            ! scrivo su file le variabili da salvare, in questo ordine: x, y, rho, P, u, v, M, S
+            ! sono rispettivamente: la coordinata x del nodo, la coordinata y del nodo (con traslazione), 
+            ! la densità al nodo, la pressione al nodo, la velocità in x al nodo, la velocità in y al nodo,
+            ! il numero di Mach al nodo e l'entropia al nodo
    end do
 
 !--------------------------------------------------------------------------------
@@ -166,6 +158,14 @@ logical::trovato
 
 !--------------------------------------------------------------------------------
  close(1)
+
+! --- AGGIUNTA: SCRITTURA FILE RECAP GENERALE ---
+ ! Apre il file in modalità "append" per aggiungere righe senza cancellare le precedenti
+ open(unit=99, file="RECAP_SIMULAZIONI.csv", status="unknown", position="append")
+ write(99, '(I0, A, I0, A, A)') k_file, ",", nnodi, ",", trim(datafile_tec)
+ close(99)
+ ! -----------------------------------------------
+
  end subroutine write_tecplot_file
  
  
